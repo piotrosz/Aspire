7d5bae7295d09249b1f2114bfa87b0dbc77c07c0
Piotr Ludwiczuk <piotrosz@gmail.com>
2025-07-25 22:21:20 +0200

    add

--- AspireApp1/AspireApp1.ApiService/BooksDataSource.cs
+++ AspireApp1/AspireApp1.ApiService/BooksDataSource.cs
@@ -6,9 +6,10 @@ public static class BooksDataSource
     {
         return
         [
-            new Book("Lektura uproszczona", "Cristina Morales", false, ReadStatus.Reading, true),
+            new Book("Lektura uproszczona", "Cristina Morales", false, ReadStatus.Read, true),
             new Book("Revolusi. Indonezja i narodziny nowoczesnego świata", "David van Reybrouck", true, ReadStatus.Read, false),
             new Book("Kandydat", "Jakub Żulczyk", false, ReadStatus.Reading, true),
+            new Book("Kwantechizm 2.0, czyli klatka na ludzi", "Andrzej Dragan", false, ReadStatus.NotRead, false)
         ];
     }
 }
